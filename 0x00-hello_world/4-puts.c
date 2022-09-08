ba84f97f658e08d5638cd9f70237654dc0506ab5
Felistus <felistusobieze@gmail.com>
2022-09-08 14:16:12 -0700

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+/**
+ * puts - Prints "Programming is like building a multilingual puzzle" 
+ * Return: 0
+ */
+#include <stdio.h>
+void puts(void)
+{
+	putchar("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
