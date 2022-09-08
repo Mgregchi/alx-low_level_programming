dcb24cefe9036566219e6b89b3de3f1f219f99c3
Felistus <felistusobieze@gmail.com>
2022-09-08 15:28:27 -0700

    updated 4-puts.c code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - Prints "Programming is like building a multilingual puzzle" 
+ * main - Prints "Programming is like building a multilingual puzzle"
  * Return: 0
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
