ff8d2b09d73318b4be64222c40fb14acedfc02ab
Felistus <felistusobieze@gmail.com>
2022-09-08 16:53:29 -0700

    6-sizes.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Prints the sizes of all data types
+ * Return: 0
+ */
+int main(void)
+{
+	int inType;
+	long longType;
+	char charType;
+	float floatType;
+	long long int longLongIntType;
