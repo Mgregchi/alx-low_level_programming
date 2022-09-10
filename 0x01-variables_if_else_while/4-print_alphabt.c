396f93c758232edb428954929140bc2c519f7541
Mgregchi <mgregchi.tk>
2022-09-10 12:53:36 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,10 +9,9 @@ int main(void)
 char alpha;
 
 for (alpha = 'a' ; alpha <= 'z' ; alpha++)
-
-if (alpha != 'q' && alpha != 'e')
+  if (alpha != 'q' && alpha != 'e')
 {
-  putchar(alpha);
+putchar(alpha);
 }
 putchar('\n');
 return (0);
