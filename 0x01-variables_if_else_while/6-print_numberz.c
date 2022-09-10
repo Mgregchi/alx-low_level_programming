9970f89176c84eb1d236e24b7ae2bf7db33bb2ca
Mgregchi <mgregchi.tk>
2022-09-10 12:49:18 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-  int num;
+int num;
 
-  for (num = 48 ; num <= 57 ; num++)
-    putchar(num);
-  putchar('\n');
-  return (0);
+for (num = 48 ; num <= 57 ; num++)
+putchar(num);
+putchar('\n');
+return (0);
 }
