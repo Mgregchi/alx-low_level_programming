5b75f497d8336d73376669abe7f1652f49f52a40
Mgregchi <mgregchi.tk>
2022-09-10 12:46:44 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-  int numb;
-  char alphab;
+int numb;
+char alphab;
 
-  for (numb = 48 ; numb <= 57 ; numb++)
-    putchar(numb);
-  for (alphab = 97 ; alphab <= 102 ; alphab++)
-    putchar(alphab);
-  putchar('\n');
-  return (0);
+for (numb = 48 ; numb <= 57 ; numb++)
+putchar(numb);
+for (alphab = 97 ; alphab <= 102 ; alphab++)
+putchar(alphab);
+putchar('\n');
+return (0);
 }
