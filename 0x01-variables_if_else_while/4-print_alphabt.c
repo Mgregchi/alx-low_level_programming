16fa1745a89662883795b092b99ff95d8637085b
Mgregchi <mgregchi.tk>
2022-09-10 12:51:30 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-  char alpha;
+char alpha;
 
-  for (alpha = 'a' ; alpha <= 'z' ; alpha++)
+for (alpha = 'a' ; alpha <= 'z' ; alpha++)
 
-    if (alpha != 'q' && alpha != 'e')
-      {
-	putchar(alpha);
-      }
-  putchar('\n');
-  return (0);
+if (alpha != 'q' && alpha != 'e')
+{
+  putchar(alpha);
+}
+putchar('\n');
+return (0);
 }
