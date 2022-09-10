7b9569a74f2bad5f7e533750b968ef8f5376e96c
Mgregchi <mgregchi.tk>
2022-09-10 12:45:27 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-  int numb;
+int numb;
 
-  for (numb = '0' ; numb <= '9' ; numb++)
-    {
-      putchar(numb);
+for (numb = '0' ; numb <= '9' ; numb++)
+{
+putchar(numb);
 
-      if (numb != '9')
-	{
-	  putchar(',');
-	  putchar(' ');
-	}
-    }
-  putchar('\n');
-  return (0);
+if (numb != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
 }
