25f586c5e965bda573f59f2d5b5e8ad70a73bc5b
Mgregchi <mgregchi.tk>
2022-09-14 06:08:13 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* 10-add.c - a function that adds two integers and returns the result.
+* @a: first input number
+* @b: second input number
+* @sum: a+b
+* Return: sum
+*/
+
+int add(int a, int b)
+{
+int sum;
+
+sum = a + b;
+return (sum);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * times_table -  a function that prints the 9 times table, starting with 0
+*/
+void times_table(void)
+{
+
+}
