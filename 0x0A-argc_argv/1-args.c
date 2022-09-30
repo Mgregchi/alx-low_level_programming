c4e99bcb7b2d0a70be07ceaf6a2238fafab180b5
Mgregchi <mgregchi.tk>
2022-09-30 06:41:09 -0700

    Create file: 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - program that prints the number of arguments passed into it.
+* Followed by newline.
+* @argc: Arguments count
+* @argv: Argument vectore
+*
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+argv=argv;
+printf("%d\n", argc);
+return (0);
+}
