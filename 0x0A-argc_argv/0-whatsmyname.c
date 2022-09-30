5036fc8e11c4d2ac78e5d2a7ec2943e168716583
Mgregchi <mgregchi.tk>
2022-09-30 05:58:23 -0700

    Remove Extra spaces

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,11 +6,11 @@
  * @argv: Argument vectore
  *
  * Return: 0
- */
+*/
 
 int main(int argc, char *argv[])
 {
-    argc = 0;
-    printf("%s\n", argv[argc]);
-    return (0);
+   argc = 0;
+   printf("%s\n", argv[argc]);
+   return (0);
 }
