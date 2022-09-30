6755f1c641d6ac5327dab3749ab87abc131774e1
Mgregchi <mgregchi.tk>
2022-09-30 08:52:58 -0700

    Fixe betty again

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,28 +2,28 @@
 #include <stdlib.h>
 
 /**
- * main - Entry Point
- * @argc: arguments
- * @argv: array pointing to arguments
- * Return: 0
- */
+* main - Entry Point
+* @argc: arguments
+* @argv: array pointing to arguments
+* Return: 0
+*/
 int main(int argc, char *argv[])
 {
-  int i, sum = 0;
+int i, sum = 0;
 
-  if (argc < 1)
-    return (0);
+if (argc < 1)
+return (0);
 
-  for (i = 1; i < argc; i++)
-    {
-      if (!atoi(argv[i]))
-	{
-	  printf("%s\n", "Error");
-	  return (1);
-	}
-      sum += atoi(argv[i]);
-    }
-  printf("%d\n", sum);
+for (i = 1; i < argc; i++)
+{
+if (!atoi(argv[i]))
+{
+printf("%s\n", "Error");
+return (1);
+}
+sum += atoi(argv[i]);
+}
+printf("%d\n", sum);
 
-  return (0);
+return (0);
 }
