5a2431d0c48d20f97748918e5e69197056bf0967
Mgregchi <git@mgregchi.tk>
2022-10-03 21:47:55 +0100

    Fix unsigned int and int comparison. Add for loop

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,19 +13,20 @@
 char *create_array(unsigned int size, char c)
 {
 char *str;
-
+unsigned int i;
 if (size == 0)
 return (NULL);
 
 str = malloc(sizeof(char) * size);
 if (str == NULL)
 return (NULL);
-for (int i = 0; i < size; i++)
+for (i = 0; i < size; i++)
 {
-str[0] = c;
+str[i] = c;
 }
 
 /** free(str);*/
 return (str);
 }
+
 
