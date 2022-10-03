cfb103127402218a7f0c0f2a5e3dc8b1375296ea
Mgregchi <git@mgregchi.tk>
2022-10-03 21:23:58 +0100

    Fix double free and betty style code in file 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,25 +2,26 @@
 #include "main.h"
 
 /**
- * create_array - function that creates an array of chars,
- * and initializes it with a specific char.
- *
- * @size: size of array
- * @c: char to initialize array with
- * Return: a pointer to the array, or NULL if it fails
- */
+* create_array - function that creates an array of chars,
+* and initializes it with a specific char.
+*
+* @size: size of array
+* @c: char to initialize array with
+* Return: a pointer to the array, or NULL if it fails
+*/
 
 char *create_array(unsigned int size, char c)
 {
-    char *str;
+char *str;
 
-    if (size == 0)
-        return (NULL);
+if (size == 0)
+return (NULL);
 
-    str = malloc(sizeof(char) * size);
-    if (str == NULL)
-        return (NULL);
-    str[0] = c;
-    free(str);
-    return (str);
+str = malloc(sizeof(char) * size);
+if (str == NULL)
+return (NULL);
+str[0] = c;
+/** free(str);*/
+return (str);
 }
+
