33e7b4340a9ee7aece8306d5d3fce88da7e0da6f
Mgregchi <git@mgregchi.tk>
2022-10-04 02:47:07 +0100

    Another method for file: 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,79 +1,40 @@
 #include <stdlib.h>
 #include "main.h"
 
-
 /**
-* _strlen - function to get length of a given string
-* @str: string to get length
-* Return: (int)
-*/
-
-int _strlen(char *str)
-{
-int len = 0;
-
-if (str == NULL)
-return (len);
-while (str[len] != '\0' && str[len] != ' ')
-{
-len++;
-}
-return (len);
-}
-
-/**
-* str_concat - function that returns a pointer
-* to a newly allocated space in memory, which contains
-* a copy of the string given as a parameter.
-*
-* @s1: char to concatenate
-* @s2: char to concatenate
-* Return: a pointer to the duplicated string,
-* or NULL if insufficient memory was available
-*/
-
+ * str_concat - Entry point
+ *@s1: string 1
+ *@s2: string 2
+ * Return: pointer should point to a newly allocated space in memory or NULL
+ */
 char *str_concat(char *s1, char *s2)
 {
-char *newstr;
-int lens1, lens2, k = 0, j = 0, size;
-lens1 = _strlen(s1);
-lens2 = _strlen(s2);
-if (s1 == NULL)
-s1 = '\0';
-if (s2 == NULL)
-s2 = '\0';
-size = lens1 + lens2;
-if (lens1 > 0 && lens2 > 0)
-size += 2;
-else if (lens1 > 0 || lens2 > 0)
-size++;
-else
-{
-size++;
+    char *strnew = NULL;
+    unsigned int i;
+    int n1;
+    int n2;
+    int count;
+
+    count = 0;
+    if (s1 == NULL)
+        s1 = "";
+    if (s2 == NULL)
+        s2 = "";
+    for (n1 = 0; s1[n1] != '\0'; n1++)
+        ;
+    for (n2 = 0; s2[n2] != '\0'; n2++)
+        ;
+    strnew = (char *)malloc((n1 + n2 + 1) * sizeof(char));
+    if (strnew == NULL)
+    {
+        return (NULL);
+    }
+    for (i = 0; s1[i] != '\0'; i++)
+        strnew[i] = s1[i];
+    for (; s2[count] != '\0'; i++)
+    {
+        strnew[i] = s2[count];
+        count++;
+    }
+    return (strnew);
 }
-newstr = malloc(sizeof(char) * size);
-if (newstr == NULL)
-return (NULL);
-while (s1[k] != '\0' && s1[k] != ' ')
-{
-newstr[k] = s1[k];
-
-k++;
-};
-if (k > 0)
-{
-newstr[k] = ' ';
-k++;
-}
-while (s2[j] != '\0' && s2[j] != ' ')
-{
-newstr[k + j] = s2[j];
-j++;
-
-}
-newstr[size] = '\0';
-
-return (newstr);
-}
-
-
---
+++ 0x0B-malloc_free/working_but_rejected/2-str_concat.c
@@ -0,0 +1,79 @@
+#include <stdlib.h>
+#include "main.h"
+
+
+/**
+* _strlen - function to get length of a given string
+* @str: string to get length
+* Return: (int)
+*/
+
+int _strlen(char *str)
+{
+int len = 0;
+
+if (str == NULL)
+return (len);
+while (str[len] != '\0' && str[len] != ' ')
+{
+len++;
+}
+return (len);
+}
+
+/**
+* str_concat - function that returns a pointer
+* to a newly allocated space in memory, which contains
+* a copy of the string given as a parameter.
+*
+* @s1: char to concatenate
+* @s2: char to concatenate
+* Return: a pointer to the duplicated string,
+* or NULL if insufficient memory was available
+*/
+
+char *str_concat(char *s1, char *s2)
+{
+char *newstr;
+int lens1, lens2, k = 0, j = 0, size;
+lens1 = _strlen(s1);
+lens2 = _strlen(s2);
+if (s1 == NULL)
+s1 = '\0';
+if (s2 == NULL)
+s2 = '\0';
+size = lens1 + lens2;
+if (lens1 > 0 && lens2 > 0)
+size += 2;
+else if (lens1 > 0 || lens2 > 0)
+size++;
+else
+{
+size++;
+}
+newstr = malloc(sizeof(char) * size);
+if (newstr == NULL)
+return (NULL);
+while (s1[k] != '\0' && s1[k] != ' ')
+{
+newstr[k] = s1[k];
+
+k++;
+};
+if (k > 0)
+{
+newstr[k] = ' ';
+k++;
+}
+while (s2[j] != '\0' && s2[j] != ' ')
+{
+newstr[k + j] = s2[j];
+j++;
+
+}
+newstr[size] = '\0';
+
+return (newstr);
+}
+
+
