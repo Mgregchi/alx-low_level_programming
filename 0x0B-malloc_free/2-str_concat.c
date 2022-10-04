d81a75643f0671f0ecd1775965108f8bd219cdc8
Mgregchi <git@mgregchi.tk>
2022-10-04 02:17:53 +0100

    Use another method for task

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include "main.h"
 
+
 /**
 * _strlen - function to get length of a given string
 * @str: string to get length
@@ -10,6 +11,9 @@
 int _strlen(char *str)
 {
 int len = 0;
+printf("NULL: %s", str);
+if (str == NULL)
+return len;
 while (str[len] != '\0' && str[len] != ' ')
 {
 len++;
@@ -31,33 +35,49 @@ return (len);
 char *str_concat(char *s1, char *s2)
 {
 char *newstr;
-int a = 0, b = 0, size;
-
+int lens1, lens2, k = 0, j = 0, size;
+lens1 = _strlen(s1);
+lens2 = _strlen(s2);
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
+size = lens1 + lens2;
+printf("SIZE IT: %d\n", size);
 
-size = _strlen(s1) + _strlen(s2) + 2;
+if (lens1 > 0 && lens2 > 0)
+size += 2;
+else if (lens1 > 0 || lens2 > 0)
+size++;
+else
+{
+size++;
+}
+printf("SIZE IT: %d\n", size);
 newstr = malloc(sizeof(char) * size);
 if (newstr == NULL)
 return (NULL);
-while (s1[a] != '\0' && s1[a] != ' ')
+while (s1[k] != '\0' && s1[k] != ' ')
 {
-newstr[a] = s1[a];
-
-a++;
+newstr[k] = s1[k];
+printf("s1[%d] = %c\n", k, s1[k]);
+k++;
 };
-newstr[a] = ' ';
-
-while (s2[b] != '\0' && s2[b] != ' ')
+if (k > 0)
 {
-newstr[a + 1 + b] = s2[b];
-
-b++;
-};
-
+newstr[k] = ' ';
+printf("KK: %d\n NEW: %d\nCHAR: _%c_\n", k, _strlen(newstr), newstr[k - 1]);
+k++;
+}
+while (s2[j] != '\0' && s2[j] != ' ')
+{
+newstr[k + j] = s2[j];
+printf("s2[%d] = %c\n", k + j, s2[j]);
+j++;
+}
 newstr[size] = '\0';
+printf("SIZE: %d\nEXC: %c", size, newstr[k + j]);
 
 return (newstr);
 }
+
