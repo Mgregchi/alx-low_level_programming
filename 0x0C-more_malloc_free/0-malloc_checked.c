a9225c33de272ef283b2bb144aefe26fc3e86dc4
Mgregchi <git@mgregchi.tk>
2022-10-06 13:39:37 +0100

    Create 0x0C-more_malloc_free, README.md, main.h, 0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* malloc_checked - a function that allocates memory using malloc.
+* @b: amount of memoery to allocate
+* Return: either void or pointer
+*/
+
+void *malloc_checked(unsigned int b)
+{
+int *ptr;
+ptr = malloc(b);
+if (b == NULL)
+exit(98);
+return (ptr);
+}
+
