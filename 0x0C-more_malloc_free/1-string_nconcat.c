40ca69b4cca4c955f55c96cb2697ca82c9cce789
Mgregchi <git@mgregchi.tk>
2022-10-06 15:01:14 +0100

    Fix betty code style and remove printf

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,53 +1,55 @@
 #include <stdlib.h>
 #include "main.h"
-#include <stdio.h>
 
-/** _strlen - function that returns length or string
- * @str: string to return length
- * Return: Int (length)
- *
- */
+
+/**
+* _strlen - function that returns length or string
+* @str: string to return length
+* Return: Int (length)
+*
+*/
 
 int _strlen(char *str)
 {
-    int count;
+int count;
 
-    for (count = 0; str[count] != '\0';)
-        count++;
-    return (count);
+for (count = 0; str[count] != '\0';)
+count++;
+return (count);
 }
 
 /**
- * string_nconcat - a function that concatenates two strings.
- * @s1: String 1 to concatenate
- * @s2: String 2 to concatenate
- * Return: Pointer | NULL
- */
+* string_nconcat - a function that concatenates two strings.
+* @s1: String 1 to concatenate
+* @s2: String 2 to concatenate
+* @n: he first n bytes of s2
+* Return: Pointer | NULL
+*/
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    char *newstr;
-    int i;
-    unsigned int lens2 = _strlen(s2), count;
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
-    if (n >= lens2)
-    {
-        n = lens2;
-        printf("Yess! n: %d\n lens2: %d\n", n, lens2);
-    }
-    newstr = malloc((_strlen(s1) + n + 1) * sizeof(char));
-    if (newstr == NULL)
-        return (NULL);
-
-    for (i = 0; s1[i] != '\0'; i++)
-        newstr[i] = s1[i];
-    for (; s2[count] != '\0' && count < n; i++)
-    {
-        newstr[i] = s2[count];
-        count++;
-    }
-    return (newstr);
+char *newstr;
+int i;
+unsigned int lens2 = _strlen(s2), count;
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+if (n >= lens2)
+{
+n = lens2;
 }
+newstr = malloc((_strlen(s1) + n + 1) * sizeof(char));
+if (newstr == NULL)
+return (NULL);
+
+for (i = 0; s1[i] != '\0'; i++)
+newstr[i] = s1[i];
+for (; s2[count] != '\0' && count < n; i++)
+{
+newstr[i] = s2[count];
+count++;
+}
+return (newstr);
+}
+
