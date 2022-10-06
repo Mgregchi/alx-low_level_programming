d4ac36c3616f48163917b710c6366cd729259430
Mgregchi <git@mgregchi.tk>
2022-10-06 11:39:02 +0100

    Fix betty code style

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -45,7 +45,6 @@ char **strtow(char *str)
 {
 int total_words = 0, b = 0, c = 0, length = 0;
 char **words, *found_word;
-
 if (str == 0 || *str == 0)
 return (NULL);
 total_words = number(str);
@@ -54,8 +53,7 @@ return (NULL);
 words = malloc((total_words + 1) * sizeof(char *));
 if (words == 0)
 return (NULL);
-for (; *str != '\0' &&  b < total_words;)
-{
+for (; *str != '\0' && b < total_words;)
 if (*str == ' ')
 str++;
 else
@@ -79,8 +77,10 @@ found_word++;
 c++;
 }
 words[b][c] = '\0';
-b++; c = 0; length = 0; str++;
-}
-}
+b++;
+c = 0;
+length = 0;
+str++;
+};
 return (words);
 }
