363190dcbd51a858c8b1c903bda965fc129d49db
Mgregchi <git@mgregchi.tk>
2022-10-06 14:41:00 +0100

    Add link to README.md

--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -9,7 +9,7 @@ freeis project, we expect you to look at this concept:**
 
 ## Resources
 - [0x0a - malloc & free - quick overview.pdf](./0x0a%20-%20malloc%20%26%20free%20-%20quick%20overview.pdf)
-- [Dynamic memory allocation in C - malloc calloc realloc free](https://)
+- [Dynamic memory allocation in C - malloc calloc realloc free](https://www.youtube.com/watch?v=xDVC3wKjS64)
 
 **man or help:**
 - malloc
@@ -26,8 +26,7 @@ freeis project, we expect you to look at this concept:**
 - All your files will be compiled on Ubuntu 20.04 LTS using gcc, using the options -Wall -Werror -Wextra -pedantic -std=gnu89
 - All your files should end with a new line
 - A README.md file, at the root of the folder of the project is mandatory
-- Your code should use the Betty style. It will be checked using betty-style.pl and betty-doc.pl
-- You are not allowed to use global variables
+- Your code should use the Betty style. It will be checked using [betty-style.pl](https://github.com/holbertonschool/Betty/blob/master/betty-style.pl) and [betty-doc.pl](https://github.com/holbertonschool/Betty/blob/master/betty-doc.pl)- You are not allowed to use global variables
 - No more than 5 functions per file
 - The only C standard library functions allowed are malloc and free. Any use of functions like printf, puts, calloc, realloc etc… is forbidden
 - You are allowed to use _putchar
--- 0x0B-malloc_free/test.c
+++ 0x0B-malloc_free/test.c
@@ -1,97 +1,12 @@
-#include <stdlib.h>
-#include "main.h"
 #include <stdio.h>
-
-/**
- * _strlen - function to get length of a given string
- * @str: string to get length
- * Return: (int)
- */
-
-int _strlen(char *str)
-{
-    int len = 0;
-    printf("NULL: %s", str);
-    if (str == NULL)
-        return len;
-    while (str[len] != '\0' && str[len] != ' ')
-    {
-        len++;
-    }
-    return (len);
-}
-
-/**
- * str_concat - function that returns a pointer
- * to a newly allocated space in memory, which contains
- * a copy of the string given as a parameter.
- *
- * @s1: char to concatenate
- * @s2: char to concatenate
- * Return: a pointer to the duplicated string,
- * or NULL if insufficient memory was available
- */
-
-char *str_concat(char *s1, char *s2)
-{
-    char *newstr;
-    int lens1, lens2, k = 0, j = 0, size;
-    lens1 = _strlen(s1);
-    lens2 = _strlen(s2);
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
-    size = lens1 + lens2;
-    printf("SIZE IT: %d\n", size);
-
-    if (lens1 > 0 && lens2 > 0)
-        size += 2;
-    else if (lens1 > 0 || lens2 > 0)
-        size++;
-    else
-    {
-        size++;
-    }
-    printf("SIZE IT: %d\n", size);
-    newstr = malloc(sizeof(char) * size);
-    if (newstr == NULL)
-        return (NULL);
-    while (s1[k] != '\0' && s1[k] != ' ')
-    {
-        newstr[k] = s1[k];
-        printf("s1[%d] = %c\n", k, s1[k]);
-        k++;
-    };
-    if (k > 0)
-    {
-        newstr[k] = ' ';
-        printf("KK: %d\n NEW: %d\nCHAR: _%c_\n", k, _strlen(newstr), newstr[k - 1]);
-        k++;
-    }
-    while (s2[j] != '\0' && s2[j] != ' ')
-    {
-        newstr[k + j] = s2[j];
-        printf("s2[%d] = %c\n", k + j, s2[j]);
-        j++;
-    }
-    newstr[size] = '\0';
-    printf("SIZE: %d\nEXC: %c", size, newstr[k + j]);
-
-    return (newstr);
-}
+#include <stdlib.h>
+#include <strings.h>
 
 int main(void)
 {
-    char *s;
+    int *ptr;
 
-    s = str_concat(NULL, NULL);
-    if (s == NULL)
-    {
-        printf("failed\n");
-        return (1);
-    }
-    printf("%s\n", s);
-    free(s);
+    *ptr = 98;
+    printf("%d\n", *ptr);
     return (0);
 }
