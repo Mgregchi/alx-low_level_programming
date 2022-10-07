d74767957d2d5c4c2c38da8fd2217f04d157f2f1
Mgregchi <git@mgregchi.tk>
2022-10-07 15:01:19 +0100

    Fix betty code style file: 101.mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,40 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-/**
-* A program that multiplies two positive numbers
-* Usage: mul num1 num2
-* num1 and num2 will be passed in base 10
-* Print the result, followed by a new line
-* If the number of arguments is incorrect, print Error,
-* followed by a new line, and exit with a status of 98
-* num1 and num2 should only be composed of digits.
-* If not, print Error, followed by a new line, and exit with a status of 98
-* You are allowed to use more than 5 functions in your file
-* You can use bc (man bc) to check your results.
-*
-* char *DEFAULT_ERROR_MSG = "Error";
-* int *DEFAULT_CODE = 98;
-*/
 
 /**
 * _error_handler - A function to handle error and exit
 * with exit code 98
-* @msg: Error msg
-* @code: Error code
 *
 */
 
-void _error_handler()
+void _error_handler(void)
 {
-/**
-*if (msg[0] == '\0')
-*    msg = DEFAULT_ERROR_MSG;
-*if (code == NULL)
-*    code = DEFAULT_CODE;
-*printf("%s\n", msg);
-*exit(code);
-*/
 printf("Error\n");
 exit(98);
 }
@@ -76,6 +51,5 @@ num2 = atoi(argv[2]);
 if (num1 && num2 == 0)
 _error_handler();
 
-printf("%d\n", (num1 * num2));
+printf("%d\n", (num1 *num2));
 }
-
