ffebd8d6456410eb682fe0c7b682cc8f97ecdfcc
Amadi Michael C <56840758+Mgregchi@users.noreply.github.com>
2022-10-10 13:55:42 +0100

    Create file: dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+* struct dog - Short description
+* @name: char - Dog name
+* @age: int - Dog age
+* @owner: char - Dog owner
+*
+* Description: Struct of a Dog with three attributes 
+*/
+struct dog
+{
+char *name;
+int *age;
+char *owner;
+};
