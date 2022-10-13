b8b83030002cea69f31daf665de3b74318c1ddfd
Mgregchi <git@mgregchi.tk>
2022-10-13 18:31:23 +0100

    Add code into 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,18 +9,15 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-va_list args;
-int sum;
 unsigned int i;
+int sum;
+va_list list;
 
-if (n == 0)
-return (0);
-va_start(args, n);
-sum = 0;
-for (i = 0; i != n; i++)
-sum += va_arg(args, int);
-va_end(args);
+va_start(list, n);
+for (i = 0, sum = 0; i < n; i++)
+sum += va_arg(list, int);
 
+va_end(list);
 return (sum);
-}
+
 
