a573553ac37c21f10ef30991c5fb64b0762b911a
Mgregchi <mgregchi.tk>
2022-10-25 15:35:48 -0700

    Local before pull

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -10,7 +10,7 @@
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-listint_t *ptr = create_node(n);
-ptr->next = &head;
+listint_t **ptr = create_node(n);
+ptr->next = head;
 return (ptr);
 }
