95d7f58af964fcb56da649c1d128ffc5a844b9a3
Amadi Michael C <56840758+Mgregchi@users.noreply.github.com>
2022-11-01 10:44:00 +0100

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,7 +8,19 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
+#include <elf.h>
+
 void check_IO_stat(int stat, int fd, char *filename, char mode);
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
@@ -16,3 +28,4 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
 #endif /* MAIN_H */
+
