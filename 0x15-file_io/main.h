9dd63b4cdf8cf823e352399df57be11bdc7b7630
Amadi Michael C <56840758+Mgregchi@users.noreply.github.com>
2022-11-01 10:34:08 +0100

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,6 +8,8 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
+void check_IO_stat(int stat, int fd, char *filename, char mode);
+
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
