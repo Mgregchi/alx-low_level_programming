fc0cef7df38846eb55224fc1083ca78d29e9fa17
Mgregchi <git@mgregchi.tk>
2022-11-03 15:26:49 +0100

    0x15-file_io local

---
+++ 0x15-file_io/file_op.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <fcntl.h>
+
+
+int main(void)
+{
+//FILE *ptr;
+
+
+int file;
+
+file = open()
+}
--- writer.py
+++
@@ -1,7 +0,0 @@
-import os
-
-with open("files.md", "r") as f:
-    #f.write("Hello world!")
-	m = str(f.read())
-	#print(m.format(key="content", valuee = "bala"))
-	print(m.format(content="baala"))
