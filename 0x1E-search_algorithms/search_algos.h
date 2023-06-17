a265e8d8eacc0bd55ca08af1658bc139051c81d8
Amadi Michael C <56840758+Mgregchi@users.noreply.github.com>
2023-06-17 02:03:00 +0100

    Update search_algos.h - Add description for  struct listint_s

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,13 +6,13 @@
 #include <stdlib.h>
 
 /**
- *
- * @n: Integer
- * @index: Index of the node in the list
- * @next: Pointer to the next node
- *
- * Description: singly linked list node structure
- */
+* struct listint_s - singly linked list node structure
+* @n: Integer
+* @index: Index of the node in the list
+* @next: Pointer to the next node
+*
+* Description: singly linked list node structure
+*/
 typedef struct listint_s
 {
 		int n;
